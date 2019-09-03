95b57c0edbede468d2be6160fc8c52943b732428
mintsberry <mintberfly@gmail.com>
2019-09-03 21:26:41 +0800

    feat: loading组件

---
+++ src/components/loading/Loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loading">
+    <img width="24" height="24" src="./loading.gif" alt="">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      
+    },
+
+    props: {
+      title: {
+        type: String,
+        default: 'Loading'
+      }
+    },
+
+    data () {
+      return {
+      };
+    },
+
+    computed: {
+      
+    },
+
+    methods: {
+      
+    },
+
+}
+</script>
+<style lang='stylus' scoped>
+  .loading
+    width 100%
+    text-align center
+    .desc
+      line-height 20px
+      font-size $font-size-small
+      color $color-text-l
+</style>
--- src/views/recommend/Recommend.vue
+++ src/views/recommend/Recommend.vue
@@ -3,30 +3,33 @@
     <Scroll  class="recommend-content" :data="discList" ref="scroll">
       <div>
         <div class="slider-wrapper">
-        <div v-if="recommend.length">
-          <Slider >
-                <div v-for="(item, index) in recommend" :key="index" >
-                <a :href="item.linkUrl">
-                  <img :src="item.picUrl" @load="imgLoad" alt="">
-                </a>
+          <div v-if="recommend.length">
+            <Slider >
+                  <div v-for="(item, index) in recommend" :key="index" >
+                  <a :href="item.linkUrl">
+                    <img :src="item.picUrl" @load="imgLoad" alt="">
+                  </a>
+                </div>
+            </Slider>
+          </div>    
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="(item, index) in discList" :key="index">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
-          </Slider>
-        </div>    
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li class="item" v-for="(item, index) in discList" :key="index">
-            <div class="icon">
-              <img width="60" height="60" v-lazy="item.imgurl" alt="">
-            </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <Loading></Loading>
       </div>
     </Scroll>
   </div>
@@ -36,11 +39,13 @@
   import {getRecommend,getDiscList} from '../../api/recommend'
   import {ERR_OK} from '../../api/config';
   import Slider from '../../components/silder/Silder.vue';
+  import Loading from '../../components/loading/Loading.vue';
   import Scroll from '../../components/scroll/Scroll.vue';
   export default {
     components: {
         Slider,
-        Scroll    
+        Scroll,
+        Loading
     },
 
     props: {
@@ -126,6 +131,10 @@
               color $color-text
             .desc
               color: $color-text-d
-
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
 
 </style>
