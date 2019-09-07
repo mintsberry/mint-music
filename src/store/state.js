b065d7447d9e40704c5077d300eacddd24c3650b
mintsberry <mintberfly@gmail.com>
2019-09-07 19:48:55 +0800

    feat: 播放器VueX

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,13 @@
+import {playMoode} from '../common/js/config'
 const state = {
   singer: {
-
-  }
+  },
+  playing: false,
+  fullScreen: false,
+  playList: [],
+  sequenceList: [],
+  mode: playMoode.sequence,
+  currentIndex: -1,
 }
 
 export default state
