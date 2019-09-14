892de3e2b9593b3959aef665d90143df99b1aaec
mintsberry <mintberfly@gmail.com>
2019-09-14 15:13:45 +0800

    feat: 搜索框样式

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    搜索
+  <div class="search">
+    <div class="search-box-wrapper">
+      <SearchBox></SearchBox>
+    </div>
   </div>
 </template>
 
 <script>
+  import SearchBox from "../../components/searchBox/SearchBox.vue";
   export default {
     components: {
-      
+      SearchBox
     },
 
     props: {
@@ -29,5 +32,5 @@
 
 }
 </script>
-<style lang='' scoped>
+<style lang='stylus' scoped>
 </style>
