c671b551111592c04bb5455f484a23fe0d17d683
mintsberry <mintberfly@gmail.com>
2019-09-18 20:55:54 +0800

    feat: 添加歌曲布局

---
+++ src/components/addSong/addSong.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="silde">
+    <div class="add-song">
+      <div class="header">
+        <h1 class="title">添加歌曲到列表</h1>
+        <div class="close">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+      <div class="search-box-wrapper"></div>
+      <div class="shortcut"></div>
+      <div class="search-result"></div>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    components: {
+      
+    },
+    props: {
+      
+    },
+    data () {
+      return {
+      };
+    },
+    computed: {
+      
+    },
+    methods: {
+      
+    },
+}
+</script>
+<style lang='stylus' scoped>
+</style>
