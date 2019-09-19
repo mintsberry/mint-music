d13283a91e62d2e3c915921dae3e0c3acee4f15f
mintsberry <mintberfly@gmail.com>
2019-09-19 23:58:48 +0800

    feat: 添加歌曲组件下历史记录

---
+++ src/components/topTip/TopTip.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="drop">
+    <div class="top-tip" v-show="showFlag">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    components: {
+      
+    },
+    props: {
+      
+    },
+    data () {
+      return {
+        showFlag = false;
+      };
+    },
+    computed: {
+      
+    },
+    methods: {
+      show() {
+        this.showFlag = true;
+      },
+      hide() {
+        this.showFlag = false;
+      },
+    },
+}
+</script>
+<style lang='stylus' scoped>
+  @import '../../common/stylus/variable.styl';
+  .top-tip
+    position fixed
+    top 0
+    width 100%
+    z-index 500
+    background $color-dialog-background
+    &.drop-enter-active,
+    &.drop-leave-active
+      transition all .3s
+    &.drop-enter,
+    &.drop-leave-to
+      transform translate3d(0,-100%,0)
+</style>
