be4646d79c5f8d91088fa6d23dbcb01dd4d084f3
mintsberry <mintberfly@gmail.com>
2019-09-26 23:48:55 +0800

    feat: 抽离Video组件，自定义control布局

---
+++ src/views/video/Video.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="video">
+    <div class="top">
+      <h1 class="title"></h1>
+    </div>
+    <video src=""></video>
+    <div class="toggle-play">
+      <i class="ckin-play"></i>
+    </div>
+    <div class="bottom">
+      <div class="time-l"></div>
+      <div class="progress"></div>
+      <div class="time-r"></div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    components: {
+      
+    },
+    props: {
+      videoUrl: {
+        type: String,
+        default: ''
+      }
+    },
+    data () {
+      return {
+      };
+    },
+    computed: {
+      
+    },
+    methods: {
+      
+    },
+}
+</script>
+<style lang='' scoped>
+</style>
